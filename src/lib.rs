07ae110ad54a8e8a3f74cfcadd51c49e929814e4
antoninhrlt <antonherault@gmail.com>
2022-12-19 22:48:28 +0100

    No direct translation for this lib

--- src/lib.rs
+++ src/lib.rs
@@ -5,34 +5,6 @@
 pub mod locales;
 pub mod strings;
 
-use locales::Locale;
-
 pub fn hello() {
     println!("hello from the lingo library !");
-}
-
-pub fn translate(string: &str, to_locale: Locale) -> String {
-    todo!()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use super::locales::{
-        countries::CountryCode,
-        languages::{Language, LanguageCode},
-    };
-
-    #[test]
-    fn translate() {
-        println!(
-            "'{}' (en) -> '{}' (fr_FR)",
-            "hello",
-            crate::translate(
-                "hello",
-                Locale(Language::new(LanguageCode::fr), CountryCode::FR)
-            )
-        );
-    }
 }
