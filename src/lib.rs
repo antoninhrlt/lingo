ba799e59324f785b12a44ceb3bb0020f31b35770
antoninhrlt <antonherault@gmail.com>
2022-12-19 23:54:57 +0100

    Lingo, the manager

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,37 @@
 pub mod locales;
 pub mod strings;
 
+use locales::Locale;
+use strings::LingoStrings;
+
 pub fn hello() {
     println!("hello from the lingo library !");
+}
+
+pub struct Lingo {
+    context_locale: Locale,
+    default_locale: Locale,
+    strings: LingoStrings,
+}
+
+impl Lingo {
+    pub fn new(context_locale: Locale, default_locale: Locale, strings: LingoStrings) -> Lingo {
+        Lingo {
+            context_locale,
+            default_locale,
+            strings,
+        }
+    }
+
+    pub fn context_locale(&self) -> &Locale {
+        &self.context_locale
+    }
+
+    pub fn default_locale(&self) -> &Locale {
+        &self.default_locale
+    }
+
+    pub fn strings(&self) -> &LingoStrings {
+        &self.strings
+    }
 }
