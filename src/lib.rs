2df8043afc64dc738fbaa1f8486fce900d8eb6af
antoninhrlt <antonherault@gmail.com>
2022-12-19 18:42:20 +0100

    First direct translation approach

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,34 @@
 
 pub mod locales;
 
+use locales::Locale;
+
 pub fn hello() {
     println!("hello from the lingo library !");
+}
+
+pub fn translate(string: &str, to_locale: Locale) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use super::locales::{
+        countries::CountryCode,
+        languages::{Language, LanguageCode},
+    };
+
+    #[test]
+    fn translate() {
+        println!(
+            "'{}' (en) -> '{}' (fr_FR)",
+            "hello",
+            crate::translate(
+                "hello",
+                Locale(Language::new(LanguageCode::fr), CountryCode::FR)
+            )
+        );
+    }
 }
